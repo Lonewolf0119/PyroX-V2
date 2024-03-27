682da4db33e951372742d0471a9a4c013cfa5c6f
AshBorn <anishsingh0119@gmail.com>
2024-03-27 16:08:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 OWNER_ID = os.getenv("OWNER_ID",6211441287)
 HANDLER = ["~", ".","!","?","@","$"]
 BARATH = "https://te.legra.ph/file/6a839cecad51014a32254.mp4"
-GROUP_ID = os.getenv("GROUP_ID", "-1002006760029)
+GROUP_ID = os.getenv("GROUP_ID", "-1002052259061)
 SOURCE = "https://github.com/Lonewolf119/PyroX-V2"
 LOG_CHAT = -1002006760029
